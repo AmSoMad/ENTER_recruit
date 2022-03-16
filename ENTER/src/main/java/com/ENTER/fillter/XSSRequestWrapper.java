c9b0e5633a59d7affbeca1f86eb68d068e64b4c2
YoonSik Lim <57971757+AmSoMad@users.noreply.github.com>
2022-03-16 16:50:50 +0900

    Update XSSRequestWrapper.java

--- ENTER/src/main/java/com/ENTER/fillter/XSSRequestWrapper.java
+++ ENTER/src/main/java/com/ENTER/fillter/XSSRequestWrapper.java
@@ -1,30 +1,3 @@
-/*
- * AIDF (AI Data Factory) version 1.0
- *
- *  Copyright ⓒ 2020 sorizava corp. All rights reserved.
- *
- *  This is a proprietary software of sorizava corp, and you may not use this file except in
- *  compliance with license agreement with sorizava corp. Any redistribution or use of this
- *  software, with or without modification shall be strictly prohibited without prior written
- *  approval of sorizava corp, and the copyright notice above does not evidence any actual or
- *  intended publication of such software.
- *
- * @package kr.co.aidf.infra.filter
- * @file XSSRequestWrapper.java
- * @author developer
- * @since 2020. 06. 23
- * @version 1.0
- * @title AIDF
- * @description
- *
- *
- * << 개정이력 (Modification Information) >>
- *
- *   수정일		수정자	수정내용
- * -------------------------------------------------------------------------------
- * 2020. 06. 23	developer		 최초생성
- * -------------------------------------------------------------------------------
- */
 package com.ENTER.fillter;
 
 import javax.servlet.http.HttpServletRequest;
